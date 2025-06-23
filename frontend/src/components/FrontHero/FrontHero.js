4a550fffabd5398037cf61aa3c8fba7e2981a5df
FilipsCalijs <filips.calijs@gmail.com>
2025-06-23 22:23:45 +0300

    add new site

---
+++ frontend/src/components/FrontHero/FrontHero.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./FrontHero.css";
+
+const FrontHeroHeader = () => {
+  return (
+    <div className="front-hero__header">
+      <div className="front-hero__logo">
+        <img
+          src="logo.png"
+          alt="Logo"
+          width={50}
+          height={50}
+        /> 
+        logo
+      </div>
+      <div className="front-hero__header-buttons">
+        <button className="header-button telegram">Telegram</button>
+        <button className="header-button support">Support</button>
+      </div>
+    </div>
+  );
+};
+
+const FrontHero = () => {
+  return (
+    <section className="front-hero">
+      <FrontHeroHeader />
+
+      <div className="front-hero__content">
+        <div className="front-hero__text">
+        <h2 className="FrontHero-Title">
+        Undress <span style={{ color: '#FF8800' }}>Photo & Video</span> online for Free
+        </h2>
+          <p className="FrontHero-Text">
+            With ClothOff, undressing photos has never been easier. Upload your
+            image, customize settings, and let our nudify AI deliver stunning
+            undressed results in seconds.
+          </p>
+          <button className="glow-button">Try for free</button>
+        </div>
+        <div className="front-hero__media">
+          <img
+            className="front-hero__image"
+            src="https://clothoff.info/wp-content/uploads/2025/06/undress-vide-ai-1.png"
+            alt="Undress AI preview"
+            width={200}
+            height={200}
+          />
+          <video
+            className="front-hero__video"
+            src="https://clothoff.info/wp-content/uploads/2025/06/video_undress-ai.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controlsList="nodownload"
+            aria-label="Undress AI video"
+            width={200}
+            height={200}
+          />
+        </div>
+      </div>
+
+    </section>
+  );
+};
+
+export default FrontHero;
