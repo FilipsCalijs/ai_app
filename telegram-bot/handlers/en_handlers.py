db55e317407b4a986218a6aa84280c487e7bec91
FilipsCalijs <filips.calijs@gmail.com>
2025-07-09 21:50:17 +0300

    delete chatbot

--- telegram-bot/handlers/en_handlers.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Dispatcher, types
-
-async def register_en_commands(dp: Dispatcher, user_id: int):
-    @dp.message(lambda message: message.from_user.id == user_id and message.text == "Hello")
-    async def en_hello(message: types.Message):
-        await message.answer("Hello! This message is from en_handlers.py")
